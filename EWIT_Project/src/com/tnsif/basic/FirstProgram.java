29ec014c6455a54a689860e912c442f1b35c45f1
prama <prama@King_of_Kings>
2025-09-26 19:12:48 +0530

    First commit

---
+++ EWIT_Project/src/com/tnsif/basic/FirstProgram.java
@@ -0,0 +1,10 @@
+package com.tnsif.basic;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+
+	}
+
+}
---
+++ EWIT_Project/src/com/tnsif/basic/SecondProgram.java
@@ -0,0 +1,10 @@
+package com.tnsif.basic;
+
+public class SecondProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Second Progran");
+
+	}
+
+}
