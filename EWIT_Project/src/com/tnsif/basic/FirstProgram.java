ddd18420378feb7a7d6b82874e8a5e71ee7368a0
prama <prama@King_of_Kings>
2025-09-29 19:30:42 +0530

    Just some simple exercise

--- EWIT_Project/src/com/tnsif/basic/FirstProgram.java
+++ EWIT_Project/src/com/tnsif/basic/FirstProgram.java
@@ -4,7 +4,15 @@ public class FirstProgram {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
-
+		Sample s=new Sample();
+		Sample s1=new Sample();
+		s.a=5;
+		s.x=6;
+		s.display();
+		s.a=7;
+		s.x=8;
+		s.display();
+		
 	}
 
 }
---
+++ EWIT_Project/src/com/tnsif/basic/Sample.java
@@ -0,0 +1,10 @@
+package com.tnsif.basic;
+
+public class Sample {
+	int a;
+	int x;
+	
+	void display() {
+		System.out.println("Value of a and x is "+a+" "+x);
+	}
+}
