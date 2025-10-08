b41326bb360e67989931410b25a6f23417df3cdd
prama <prama@King_of_Kings>
2025-10-08 20:03:13 +0530

    Scanner class

---
+++ EWIT_Project/src/com/tnsif/scanner/School.java
@@ -0,0 +1,5 @@
+package com.tnsif.scanner;
+
+public class School {
+
+}
---
+++ EWIT_Project/src/com/tnsif/scanner/UserName.java
@@ -0,0 +1,21 @@
+package com.tnsif.scanner;
+import java.util.Scanner;
+
+public class UserName {
+	public static void main(String args[]) {
+		System.out.println("Enter your Name: ");
+		Scanner sc = new Scanner(System.in);
+		String name = sc.nextLine();
+		System.out.println("Hello "+name+", Enter Your Number please!");
+		
+		long PhoneNum = sc.nextLong();
+		System.out.println("Enter the Gender typing F or M");
+		char gender = sc.next().charAt(1);
+		System.out.println("Gender is "+gender);
+		System.out.println("Phone Number: "+PhoneNum);
+		
+		School s=new School();
+		
+	}
+
+}
