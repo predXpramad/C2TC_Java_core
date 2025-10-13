55b9f35d827042a51f43cec0178cb4148dcc83bd
prama <prama@King_of_Kings>
2025-10-13 19:55:45 +0530

    static and Final class examples

--- EWIT_Project/src/com/tnsif/scanner/School.java
+++ EWIT_Project/src/com/tnsif/scanner/School.java
@@ -1,5 +1,26 @@
 package com.tnsif.scanner;
 
 public class School {
-
+	private static int s;
+	private int b;
+	private static int a;
+	
+	public static void m1() {
+		//non-static members can't be accessed in static method
+		System.out.println(a);
+	}
+	
+	public void m2() {
+		System.out.println(b);
+		System.out.println(a);
+		m1();
+	}
+	
+	static{
+		System.out.println("Hi, welcome to your First class.");
+	}
+	public static void main(String args[]) {
+		
+		
+	}
 }
--- EWIT_Project/src/com/tnsif/scanner/UserName.java
+++ EWIT_Project/src/com/tnsif/scanner/UserName.java
@@ -16,6 +16,7 @@ public class UserName {
 		
 		School s=new School();
 		
+		
 	}
 
 }
